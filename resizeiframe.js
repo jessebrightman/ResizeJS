e5f328adc91baf816bbf1122d8b62e8626716013
jessebrightman <jbrightman@my.bcit.ca>
2017-05-16 09:23:16 -0700

    Create resizeiframe.js

---
+++ resizeiframe.js
@@ -0,0 +1,6 @@
+<script>
+			jQuery(window).on('resize', function(){
+			 var obj = document.getElementById('youriframehere');
+			 obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+			});
+</script>
